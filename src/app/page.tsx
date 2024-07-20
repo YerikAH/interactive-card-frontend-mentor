7eb8ba7782ed920c629207b556033982f0b2ff0f
Harvey Yerik <yerik05vh@gmail.com>
2024-07-20 11:13:56 -0500

    :wrench: Update configuration files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,47 @@
 import Image from "next/image";
+import banner from "@/assets/bg-main-mobile.png";
 
 export default function Home() {
   return (
-    <main className="">
-      <div></div>
-      <div>
-        <div>
-          <label htmlFor="" className="font-spaceGrotesk">
-            CARDHOLDER NAME
-          </label>
-          <input type="text" placeholder="e.g. Jane Applessed" />
-        </div>
-        <div>
-          <label htmlFor="">CARD NUMBER</label>
-          <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
-        </div>
+    <main className="w-full lg:grid-cols-2 grid">
+      <div className="w-full">
+        <Image
+          className="w-full object-cover h-60"
+          src={banner}
+          loading="lazy"
+          alt="banner"
+        />
+      </div>
+      <div className="w-full px-4">
+        <form>
+          <div>
+            <label
+              htmlFor=""
+              className="text-sm text-very-dark-violet tracking-widest"
+            >
+              CARDHOLDER NAME
+            </label>
+            <input
+              type="text"
+              placeholder="e.g. Jane Applessed"
+              className="text-base border rounded-md px-4 py-2 block w-full"
+            />
+          </div>
+          <div>
+            <label htmlFor="">CARD NUMBER</label>
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+          </div>
 
-        <div>
-          <label htmlFor="">EXP. DATE (MM/YY) </label>
-          <div></div>
-          <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
-        </div>
+          <div>
+            <label htmlFor="">EXP. DATE (MM/YY) </label>
+            <div></div>
+            <input type="text" placeholder="e.g. 1234 5678 9123 0000" />
+          </div>
 
-        <button>Confirm</button>
+          <button className="text-white w-full bg-very-dark-violet rounded-md px-4 py-2">
+            Confirm
+          </button>
+        </form>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,8 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        spaceGrotesk: ["SpaceGrotesk", "sans-serif"],
+      colors: {
+        "light-grayish-violet": "hsl(270, 3%, 87%)",
+        "dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "very-dark-violet": "hsl(278, 68%, 11%)",
       },
     },
   },
