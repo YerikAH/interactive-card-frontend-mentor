295c38e75ae35fd19f02f4521abc6137a04e2e10
yerik05vh <yerik05vh@gmail.com>
2024-07-21 17:58:47 +0000

    :sparkles: add new useWindowSize hook.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { FormCard, ViewCard } from "@/components";
 
 export default function Home() {
   return (
-    <main className="w-full lg:grid-cols-2 grid">
+    <main className="w-full lg:grid-cols-[300px_auto] grid">
       <ViewCard />
       <FormCard />
     </main>
